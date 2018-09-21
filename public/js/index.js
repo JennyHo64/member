e6b2b9e7d607d47df54796113f34826771236eac
JennyHo64 <je64yyy@gmail.com>
2018-09-21 16:54:42 +0800

    180921_member_modify

--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,69 @@
 $(document).ready(function(){
-   
+   getMembers();
 });
 
+//2.取得會員資料
+function getMembers(){
+    let apiUrl = 'http://localhost:3000/api/take';
+    let reqInit = {
+        "headers": new Headers({'Content-Type': 'application/json'}),//header的H要大寫!!!!!!
+        "method": 'GET'
+    }
+
+    return window.fetch(apiUrl,reqInit).then(function(res){
+        res.json().then(function(resJson){
+            let docs= resJson.docs;
+            //let docs = resJson['docs']//同等於上一行
+            docs.map(function(doc){
+                let id=doc.id;
+                let name=doc.name;
+                let age=doc.age;
+                let sex=doc.sex;
+                let trElement = `<tr data-memberId="${id}">
+                                <th id="th-name">${name}</th>
+                                <th id="th-age">${age}</th>
+                                <th id="th-sex">${sex}</th>
+                                <td>
+                                    <button type="button" id="delete">刪除</button>
+                                    <button type="button" id="modify" data-toggle="modal" data-target="#update-modal">修改</button>
+                                </td>
+                            </tr>`;
+                $('#member-list').append(trElement);   
+            })
+            console.log(resJson);//resJson是處理完的結果
+        })
+    })
+}
+
+
+
+//1.新增成員
 $(document).on('click','#submit',function(){
     let name= $('#name').val();//如果val內沒有輸入值代表要取name的職.若val內有直則改變name元友直
     //宣告變數可用 var let,let只能在該function中作用
     let age = $('#age').val();
     let sex = $('#sex').val();
 
-    let appurl = 'http://localhost:3000/api/creat?'+'name_key='+name+'&age_key='+age+'&sex_key='+sex;//query寫法
+    let apiurl = 'http://localhost:3000/api/creat';
+    // ?'+'name_key='+name+'&age_key='+age+'&sex_key='+sex;//query寫法
     let reqInit = {
         "headers": new Headers({'Content-Type': 'application/json'}),//header的H要大寫!!!!!!
-        "method": 'GET',
+        "method": 'POST',
+        "body":JSON.stringify({
+            "name_key":name,
+            "age_key":age,
+            "sex_key":sex,
+        })
+        // "headers": new Headers({'Content-Type': 'application/json'}),//header的H要大寫!!!!!!
+        // "method": 'GET',
     }
-    // let reqInit = {
-    //     "headers": new Headers({'Content-Type': 'application/json'}),//header的H要大寫!!!!!!
-    //     "method": 'POST',
-    //     "body":JSON.stringify({
-    //         "name_key":name,
-    //         "age_key":age,
-    //         "sex_key":sex
-    //     })
-    
+   
 
     console.log('name: '+name+' age: '+age+' sex: '+sex);
     console.log(`name:${name} age:${age} sex:${sex}`);//模板字串`
 //以上是前端把資料丟回給後端
 
-    return window.fetch(appurl,reqInit).then(function(res){
+    return window.fetch(apiurl,reqInit).then(function(res){
         res.json().then(function(resJson){
             let idInFuc=resJson.id;
             let nameInFuc=resJson.name;
@@ -35,9 +71,9 @@ $(document).on('click','#submit',function(){
             let sexInFuc=resJson.sex;
 
             let trElement = `<tr data-memberId="${idInFuc}">
-                                <th>${nameInFuc}</th>
-                                <th>${ageInFuc}</th>
-                                <th>${sexInFuc}</th>
+                                <th id="th-name">${nameInFuc}</th>
+                                <th id="th-age">${ageInFuc}</th>
+                                <th id="th-sex">${sexInFuc}</th>
                             </tr>`;
             console.log(resJson);//resJson是處理完的結果
             $('#member-list').append(trElement);
@@ -49,6 +85,15 @@ $(document).on('click','#submit',function(){
 });
 
 
+$(document).on('click','#modify',function(){
+    let thName=$(this).parent().parent().find('#th-name').text();
+    let thAge=$(this).parent().parent().find('#th-age').text();
+    let thSex=$(this).parent().parent().find('#th-sex').text();
+
+    $('#name_m').val(thName);
+    $('#age_m').val(thAge);
+    $('#sex_m').val(thSex);
+})
 
 
 
--- routes/api.js
+++ routes/api.js
@@ -4,16 +4,18 @@ var Cloudant = require('@cloudant/cloudant'); //使用cloudant資料庫的寫法
 var username = '290882c8-24eb-4919-8987-f6f8b8fdfab8-bluemix'; 
 var password = 'bd7d946c9b83b6663b65ca6ab829345c272961c5beec2869d37dd6803469bf3f';
 
-router.get('/creat',function(req,res){
+//1.新增成員
+router.post('/creat',function(req,res){
     //let db=Cloudant(username, password);
     
-    let name = req.query.name_key; //query可改成body或params
-    let age = req.query.age_key;
-    let sex = req.query.sex_key;
+    let name = req.body.name_key; //query可改成body或params
+    let age = req.body.age_key;
+    let sex = req.body.sex_key;
     let pushData = {
         "name": name,
         "age" : age,
-        "sex" : sex
+        "sex" : sex,
+        "timeStemp":Date.now()
     }//json格式 key要""
 
     Cloudant({account:username, password:password}, function(err, cloudant) {
@@ -36,5 +38,35 @@ router.get('/creat',function(req,res){
 
 });
 
-module.exports = router; //後端城市寫完都需要加這一行供其他城市使用
+//2.取得會員資料
+
+router.get('/take',function(req,res){
+  Cloudant({account:username, password:password}, function(err, cloudant) {
+    if (err) {
+      return console.log('Failed to initialize Cloudant: ' + err.message);
+    }
+  
+    var db = cloudant.db.use("member");
+      let quary={
+        "selector":{
+          "_id":{
+            "$gt":"0"
+          } 
+        },
+        "sort": [{"timeStemp": "asc"}]
+      }
+
+
+    db.find(quary,function(err, data) {
+      // The rest of your code goes here. For example:
+      if (err) {
+        return console.log('Failed to insert: ' + err.message);
+      }
+      console.log(data);
+      res.status(200).json(data);
+    });
+  });
+})
+
+module.exports = router; //後端程式寫完都需要加這一行供其他程式使用
 
