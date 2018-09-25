2aac7cd60e2b54416ddee1721aff82e0c8857c26
JennyHo64 <je64yyy@gmail.com>
2018-09-25 18:10:43 +0800

    180925_modify_finish

--- public/js/index.js
+++ public/js/index.js
@@ -1,101 +1,147 @@
-//2.取得會員資料
+(function(){
+    var memberId;
 
-$(document).ready(function(){
-   getMembers();
-});
+    //2.取得會員資料
 
-function getMembers(){
-    let apiUrl = 'http://localhost:3000/api/take';  //為何麼不能在/creat??
-    let reqInit = {
-        "headers": new Headers({'Content-Type': 'application/json'}),//header的H要大寫!!!!!!
-        "method": 'GET'
+    $(document).ready(function(){
+    getMembers();
+    });
+
+    function getMembers(){
+        let apiUrl = window.location.origin + '/api/take';  
+        let reqInit = {
+            "headers": new Headers({'Content-Type': 'application/json'}),//header的H要大寫!!!!!!
+            "method": 'GET'
+        }
+
+        return window.fetch(apiUrl,reqInit).then(function(res){
+            res.json().then(function(resJson){
+                let docs= resJson.docs;//要從哪裡看docs??
+                //let docs = resJson['docs']//同等於上一行
+                docs.map(function(doc){  //map是一個迴圈的形式取所有資料的值
+                    let id=doc._id;  //_id是資料庫的格式"_id"
+                    let name=doc.name;
+                    let age=doc.age;
+                    let sex=doc.sex;
+                    let trElement = `<tr id="${id}">
+                                        <th id="th-name">${name}</th>
+                                        <th id="th-age">${age}</th>
+                                        <th id="th-sex">${sex}</th>
+                                    <td>
+                                        <button type="button" id="delete">刪除</button>
+                                        <button type="button" id="modify" data-toggle="modal" data-target="#update-modal">修改</button>
+                                    </td>
+                                </tr>`;
+                    $('#member-list').append(trElement);   
+                })
+                console.log(resJson);//resJson是處理完的結果
+            })
+        })
     }
 
-    return window.fetch(apiUrl,reqInit).then(function(res){
-        res.json().then(function(resJson){
-            let docs= resJson.docs;//要從哪裡看docs??
-            //let docs = resJson['docs']//同等於上一行
-            docs.map(function(doc){  //map是一個迴圈的形式取所有資料的值
-                let id=doc.id;
-                let name=doc.name;
-                let age=doc.age;
-                let sex=doc.sex;
-                let trElement = `<tr data-memberId="${id}">
-                                    <th id="th-name">${name}</th>
-                                    <th id="th-age">${age}</th>
-                                    <th id="th-sex">${sex}</th>
+
+
+    //1.新增成員
+    $(document).on('click','#submit',function(){
+        let name= $('#name').val();//如果val內沒有輸入值代表要取name的職.若val內有直則改變name原有值
+        //宣告變數可用 var let,let只能在該function中作用
+        let age = $('#age').val();
+        let sex = $('#sex').val();
+
+        let apiurl = window.location.origin + '/api/creat';
+        // ?'+'name_key='+name+'&age_key='+age+'&sex_key='+sex;//query寫法
+        let reqInit = {
+            "headers": new Headers({'Content-Type': 'application/json'}),//header的H要大寫!!!!!!
+            "method": 'POST',
+            "body":JSON.stringify({
+                "name_key":name,
+                "age_key":age,
+                "sex_key":sex,
+            })
+            // "headers": new Headers({'Content-Type': 'application/json'}),//header的H要大寫!!!!!!
+            // "method": 'GET',
+        }
+    
+
+        console.log('name: '+name+' age: '+age+' sex: '+sex);
+        console.log(`name:${name} age:${age} sex:${sex}`);//模板字串`
+    //以上是前端把資料丟回給後端
+
+        return window.fetch(apiurl,reqInit).then(function(res){
+            res.json().then(function(resJson){   //res是後端送回給前端的結果  .json()把res解開  存到resJson
+                let idInFuc=resJson.id;
+                let nameInFuc=resJson.name;
+                let ageInFuc=resJson.age;
+                let sexInFuc=resJson.sex;
+
+                let trElement = `<tr id="${idInFuc}">
+                                    <th id="th-name">${nameInFuc}</th>
+                                    <th id="th-age">${ageInFuc}</th>
+                                    <th id="th-sex">${sexInFuc}</th>
                                 <td>
                                     <button type="button" id="delete">刪除</button>
                                     <button type="button" id="modify" data-toggle="modal" data-target="#update-modal">修改</button>
                                 </td>
-                            </tr>`;
-                $('#member-list').append(trElement);   
+                                </tr>`;
+                console.log(resJson);//resJson是處理完的結果
+                $('#member-list').append(trElement);
             })
-            console.log(resJson);//resJson是處理完的結果
+            
+            console.log(res);  //這裡是後端把結果丟給前端(ex:顯示出剛剛前面輸入的結果)
+
         })
+    });
+
+    //3.修改
+    $(document).on('click','#modify',function(){
+        let thName=$(this).parent().parent().find('#th-name').text();  //$(this)在瀏覽器的F12裡面'Element'頁面用watch看  要在debug模式下
+        let thAge=$(this).parent().parent().find('#th-age').text();   //this是指click到#modify
+        let thSex=$(this).parent().parent().find('#th-sex').text();
+
+        memberId=$(this).parent().parent().attr('id');   //this是指click到#modify  //全域變數在最前面已經宣告過了
+
+
+        $('#name_m').val(thName);
+        $('#age_m').val(thAge);
+        $('#sex_m').val(thSex);
     })
-}
 
+    $(document).on('click','#saveChange',function(){
+        let modalName= $('#name_m').val();
+        let modalAge= $('#ag_m').val();
+        let modalsex= $('#sex_m').val();
 
+        let apiUrl=window.location.origin + '/api/update';
+        let reqInit={
+            "headers": new Headers({'Content-Type': 'application/json'}),//header的H要大寫!!!!!!
+            "method": 'PUT', // 代表修改
+            "body":JSON.stringify({
+                "name_key":modalName,
+                "age_key":modalAge,
+                "sex_key":modalsex,
+                "id_key":memberId  //memberId在修改跟刪除的時候都要用到，是db的唯一識別
+            })
+        }
+        return window.fetch(apiUrl,reqInit).then(function(res){
+            res.json().then(function(resJson){
+                let name = resJson.name;
+                let age = resJson.age;
+                let sex = resJson.sex;
+                let id = resJson._id;
 
-//1.新增成員
-$(document).on('click','#submit',function(){
-    let name= $('#name').val();//如果val內沒有輸入值代表要取name的職.若val內有直則改變name原有值
-    //宣告變數可用 var let,let只能在該function中作用
-    let age = $('#age').val();
-    let sex = $('#sex').val();
+                $('#' + id).find('#th-name').text(name);   //在選擇棄裡面'#'代表html的id
+                $('#' + id ).find('#th-age').text(age);
+                $('#' + id ).find('#th-sex').text(sex);
+            }).then(function(){
+                $('#update-modal').modal('hide')   //.then()讓程式執行完前面後馬上接著執行這行
+            })
 
-    let apiurl = 'http://localhost:3000/api/creat';
-    // ?'+'name_key='+name+'&age_key='+age+'&sex_key='+sex;//query寫法
-    let reqInit = {
-        "headers": new Headers({'Content-Type': 'application/json'}),//header的H要大寫!!!!!!
-        "method": 'POST',
-        "body":JSON.stringify({
-            "name_key":name,
-            "age_key":age,
-            "sex_key":sex,
+            
         })
-        // "headers": new Headers({'Content-Type': 'application/json'}),//header的H要大寫!!!!!!
-        // "method": 'GET',
-    }
-   
+    });
 
-    console.log('name: '+name+' age: '+age+' sex: '+sex);
-    console.log(`name:${name} age:${age} sex:${sex}`);//模板字串`
-//以上是前端把資料丟回給後端
 
-    return window.fetch(apiurl,reqInit).then(function(res){
-        res.json().then(function(resJson){
-            let idInFuc=resJson.id;
-            let nameInFuc=resJson.name;
-            let ageInFuc=resJson.age;
-            let sexInFuc=resJson.sex;
-
-            let trElement = `<tr data-memberId="${idInFuc}">
-                                <th id="th-name">${nameInFuc}</th>
-                                <th id="th-age">${ageInFuc}</th>
-                                <th id="th-sex">${sexInFuc}</th>
-                            </tr>`;
-            console.log(resJson);//resJson是處理完的結果
-            $('#member-list').append(trElement);//member-list是哪來的?
-        })
-        
-        console.log(res);  //這裡是後端把結果丟給前端(ex:顯示出剛剛前面輸入的結果)
-
-    })
-});
-
-//3.修改
-$(document).on('click','#modify',function(){
-    let thName=$(this).parent().parent().find('#th-name').text();  //$(this)在瀏覽器的F12裡面用watch看
-    let thAge=$(this).parent().parent().find('#th-age').text();
-    let thSex=$(this).parent().parent().find('#th-sex').text();
-
-    $('#name_m').val(thName);
-    $('#age_m').val(thAge);
-    $('#sex_m').val(thSex);
-})
-
+}());
 
 
 
--- routes/api.js
+++ routes/api.js
@@ -31,7 +31,7 @@ router.post('/creat',function(req,res){
               return console.log('Failed to insert: ' + err.message);
           }
           let id=data.id;
-          pushData.id=id;  //意思是在push裡面加一個"id":id like上面14~16行
+          pushData.id=id;  //意思是在push裡面加一個"id":id like上面14~16行--->pushData.id的id
           res.status(200).json(pushData);//將被新增的資料帶上那個唯一識別的id回傳給前端，告訴前端說這筆資料已經新增進資料庫了
           
         });
@@ -54,7 +54,7 @@ router.get('/take',function(req,res){
             "$gt":"0"
           } 
         },
-        "sort": [{"timeStemp": "asc"}]
+        "sort": [{"timeStemp": "asc"}]   //到BD新增index的地方，先在左上修改之後再按新增
       }
 
     
@@ -64,10 +64,53 @@ router.get('/take',function(req,res){
         return console.log('Failed to Find: ' + err.message);
       }
       console.log(data);
-      res.status(200).json(data);
+      res.status(200).json(data);  //把處理好的資料存在data用json的方式包裝在res回覆給前端
     });
   });
 })
 
-module.exports = router; //後端程式寫完都需要加這一行供其他程式使用
 
+router.put('/update',function(req,res){
+  let name=req.body.name_key;
+  let age=req.body.age_key;
+  let sex=req.body.sex_key;
+  let id=req.body.id_key;
+  Cloudant({account:username, password:password}, function(err, cloudant) {
+    if (err) {
+      return console.log('Failed to initialize Cloudant: ' + err.message);
+    }
+    var db = cloudant.db.use("member");
+      let quary={
+        "selector":{
+          "_id":{
+            "$eq":id
+          } 
+        },
+        "sort": [{"timeStemp": "asc"}]   //到BD新增index的地方，先在左上修改之後再按新增
+      }
+
+    
+    db.find(quary,function(err, data) {
+      // The rest of your code goes here. For example:
+      if (err) {
+        return console.log('Failed to Find: ' + err.message);
+      }
+      console.log(data);
+      let doc=data.docs[0];
+      doc.name = name;
+      doc.age = age;
+      doc.sex = sex;
+      doc.updateTime = Date.now();  //updateTime要跟資料庫的query指令一樣
+
+      db.insert(doc,function(err,result){
+        if (err) {
+          return console.log('Failed to insert: ' + err.message);
+        }
+        console.log();
+        res.status(200).json(doc);
+      })
+    });
+  })
+})
+
+module.exports = router; //後端程式寫完都需要加這一行供其他程式使用
